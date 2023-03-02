ca6541b8d709c64300002c7cf553db05724a95ee
Sudhir Thanki <sudhir-c@agrisync.com>
2023-03-02 19:19:29 +0530

    Star Print SDK - Sample

---
+++ testStarSdk/testStarSdk.Android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using AndroidX.Core.Content;
+using Android;
+using AndroidX.Core.App;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+
+namespace testStarSdk.Droid
+{
+    [Activity(Label = "testStarSdk", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            await Permissions.RequestAsync<BLEPermission>();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void CheckPermission()
+        {
+            var permissionList = new List<string>();
+
+            if (ContextCompat.CheckSelfPermission(this, "android.permission.BLUETOOTH_CONNECT") == Permission.Granted)
+            {
+                if(Build.VERSION.SdkInt <=  BuildVersionCodes.R)
+                {
+
+                }
+                else
+                {
+                    permissionList.Add("android.permission.BLUETOOTH_CONNECT");
+                    ActivityCompat.RequestPermissions(this, permissionList.ToArray(), 2);
+                }
+                // Manifest.Permission.BluetoothConnect)
+            }
+        }
+    }
+
+
+    public class BLEPermission : Xamarin.Essentials.Permissions.BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
+        {
+            ("android.permission.BLUETOOTH_CONNECT",true)
+            //(Android.Manifest.Permission.BluetoothScan, true),
+            //(Android.Manifest.Permission.BluetoothConnect, true)
+        }.ToArray();
+    }
+}
---
+++ testStarSdk/testStarSdk/MainPage.xaml.cs
@@ -0,0 +1,177 @@
+﻿using StarIoWrapper;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace testStarSdk
+{
+    interface IListViewItem
+    {
+        string Text { get; }
+
+        string DetailText { get; }
+
+        Color TextColor { get; }
+
+        Color DetailTextColor { get; }
+    }
+
+    class ListViewItemCollection : ObservableCollection<IListViewItem>
+    {
+        public string Header { get; }
+
+        public ListViewItemCollection(string header)
+        {
+            this.Header = header;
+        }
+
+      
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        private ListViewItemCollection listViewItemCollection;
+
+
+        private ObservableCollection<ListViewItemCollection> observableCollection;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.observableCollection = new ObservableCollection<ListViewItemCollection>();
+            this.listViewItemCollection = new ListViewItemCollection("List");
+            this.observableCollection.Add(this.listViewItemCollection);
+
+            this.listView.ItemsSource = this.observableCollection;
+        }
+        async void RefreshPort_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await RefreshPortInfo();
+        }
+
+        private void Init()
+        {
+            bool userInteractionEnabled = false;
+
+            Dictionary<StarIoExtEmulation, List<bool>> userInteractionEnabledDictionary = new Dictionary<StarIoExtEmulation, List<bool>>()
+            {
+                {StarIoExtEmulation.StarPRNT,      new List<bool>() {userInteractionEnabled, userInteractionEnabled, userInteractionEnabled, userInteractionEnabled}},
+                {StarIoExtEmulation.StarLine,      new List<bool>() {userInteractionEnabled, userInteractionEnabled, userInteractionEnabled, userInteractionEnabled}},
+                {StarIoExtEmulation.StarGraphic,   new List<bool>() {userInteractionEnabled, userInteractionEnabled, userInteractionEnabled, userInteractionEnabled}},
+                {StarIoExtEmulation.EscPos,        new List<bool>() {userInteractionEnabled, userInteractionEnabled, userInteractionEnabled, userInteractionEnabled}},
+                {StarIoExtEmulation.EscPosMobile,  new List<bool>() {userInteractionEnabled, false,                  userInteractionEnabled, userInteractionEnabled}},
+                {StarIoExtEmulation.StarDotImpact, new List<bool>() {userInteractionEnabled, userInteractionEnabled, userInteractionEnabled, userInteractionEnabled}},
+                {StarIoExtEmulation.StarPRNTL,     new List<bool>() {userInteractionEnabled, userInteractionEnabled, userInteractionEnabled, userInteractionEnabled}}
+            };
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //if (this.didAppear == false)
+            //{
+                await RefreshPortInfo();
+
+               // didAppear = true;
+            //}
+        }
+
+        public async Task RefreshPortInfo()
+        {
+            Dictionary<string, string> targetDictionary;
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                targetDictionary = new Dictionary<string, string>
+                {
+                    {"LAN",                  "TCP:"},
+                    {"Bluetooth",            "BT:"},
+                    {"Bluetooth Low Energy", "BLE:"},
+                    {"USB",                  "USB:"},
+                    {"All",                  ""}
+                };
+            }
+            else
+            {
+                targetDictionary = new Dictionary<string, string>
+                {
+                    {"LAN",       "TCP:"},
+                    {"Bluetooth", "BT:"},
+                    {"USB",       "USB:"},
+                    {"All",       ""}
+                };
+            }
+
+            try
+            {
+                string target = targetDictionary[await DisplayActionSheet("Select Interface.", "Cancel", null, targetDictionary.Keys.ToArray())];
+
+                //this.contentPageComponent.UserInteractionEnabled = false;
+
+                listViewItemCollection.Clear();
+
+                try
+                {
+                    if (target == "")
+                    {
+                        Communication.SearchPrinter(SearchPrinterAction);
+                    }
+                    else
+                    {
+                        Communication.SearchPrinter(target, SearchPrinterAction);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            catch
+            {
+                await Navigation.PopAsync(true);
+            }
+        }
+
+        public void SearchPrinterAction(IList<PortInfo> portInfoList)
+        {
+            foreach (PortInfo portInfo in portInfoList)
+            {
+                ListViewItem listViewItem = new ListViewItem(portInfo);
+
+                this.listViewItemCollection.Add(listViewItem);
+            }
+
+            //this.contentPageComponent.UserInteractionEnabled = true;
+        }
+    }
+
+
+    public class ListViewItem : IListViewItem
+    {
+        public PortInfo PortInfo { get; }
+
+        public string Text { get; }
+
+        public Color TextColor { get; }
+
+        public string DetailText { get; }
+
+        public Color DetailTextColor { get; }
+
+        public ListViewItem(PortInfo portInfo)
+        {
+            this.PortInfo = portInfo;
+
+            this.Text = this.PortInfo.ModelName;
+            this.TextColor = Color.DodgerBlue;
+            this.DetailText = this.PortInfo.PortName;
+            this.DetailTextColor = Color.DodgerBlue;
+        }
+    }
+}
